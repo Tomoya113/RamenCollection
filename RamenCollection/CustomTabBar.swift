98c50ef317f63d3ac64106491f670f8c33ff3aa0
Tomoya Tanaka <tomoya113.code@gmail.com>
2020-10-04 21:45:01 +0900

    add tabbar

---
+++ RamenCollection/CustomTabBar.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTabBar.swift
+//  RamenCollection
+//
+//  Created by tomoya tanaka on 2020/10/04.
+//  Copyright © 2020 Tomoya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBar: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // NOTE: SwiftのUIcolorのRGB値は、256段階ではなく、0~1.0までの値で指定するようになっている
+        self.tabBar.tintColor = UIColor(red: 249/255, green: 171/255, blue: 24/255, alpha: 1.0)
+        
+    }
+}
